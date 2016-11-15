49e76ff3cb88194ffb0909d898e3d6e2f7f93bea
santiagolv <santiagolv@users.noreply.github.com>
2016-11-15 00:34:56 -0600

    Pending Characters Edition

---
+++ app/models/movie.rb
@@ -0,0 +1,13 @@
+class Movie < ActiveRecord::Base
+  # director_id: must be present
+  validates :director_id, :presence=> true
+  # title: must be present; must be unique in combination with year
+  validates :title, :presence=> true, :uniqueness => {:scope =>[:year, :duration]}
+  # year: must be integer between 1870 and 2050
+validates :year, :numericality=> {only_integer: true, :grater_than_or_equal_to =>1870 ,:less_than_or_equal_to => 2015}
+  # duration: must be integer between 0 and 2764800
+validates :year, :numericality=> {only_integer: true, :grater_than_or_equal_to =>0 ,:less_than_or_equal_to => 2764800}
+  # description: no rules
+
+  # image_url: no rules
+end
